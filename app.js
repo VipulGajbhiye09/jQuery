2071ee15b8fe596c38cbaf4fc540adc85a42eec1
VipulGajbhiye09 <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-10-29 23:32:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+//jQuery is a JavaScript Library. it simplifies JavaScript programming.
+
+//$ = jQuery("") = document.querySelector("")
+
+
+//set info
+//$("h1");
+//$("h1").css("color","green");
+
+//retreive info
+//console.log($("h1").css("color"));
+
+$("h1").addClass("title-size");
