8acfe79dec3c513624f89b15510151a41697fe32
VipulGajbhiye09 <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-10-30 23:15:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,4 +10,11 @@
 //retreive info
 //console.log($("h1").css("color"));
 
+//add a class
 $("h1").addClass("title-size");
+
+//remove a class
+$("h1").removeClass("title-size");
+
+//Check if element has a class
+//console.log($("h1").hasClass("title-size"));
