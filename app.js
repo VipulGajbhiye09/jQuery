133bb9093ae440dc1b38af027a9412bfa69ecd44
VipulGajbhiye09 <111264723+VipulGajbhiye09@users.noreply.github.com>
2022-10-30 23:18:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,3 +18,14 @@ $("h1").removeClass("title-size");
 
 //Check if element has a class
 //console.log($("h1").hasClass("title-size"));
+
+//Text Manipulation
+
+$("h1").text("GoodBye");   //alter only text content
+$("h1").html("<em>Hey</em>"); //alter all content in a tag
+
+//Event Listener
+
+$("button").click(function(){
+  $("h1").css("color","purple");
+});
